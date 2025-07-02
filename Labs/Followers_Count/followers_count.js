deb5dd27685f8990a10a44f733618141a5e6bebf
Mikhail McCatty <mikhailmccatty12@gmail.com>
2025-07-02 19:24:35 -0400

    add Practice Task to Followers Count lab

--- Labs/Followers_Count/followers_count.js
+++ Labs/Followers_Count/followers_count.js
@@ -5,15 +5,20 @@ function increaseCount() {
     checkCountValue();
 }
 
+function resetCount() {
+    count = 0;
+    displayCount();
+    alert("Your followers count has been reset.");
+}
 function displayCount() {
     document.getElementById('countDisplay').innerHTML = count;
 }
 
 function checkCountValue() {
     if (count === 10) {
-        alert("Your Instagram account has gained 10 followers.")
+        alert("Your account has gained 10 followers.")
     } else if (count === 20) {
-        alert("Your Instagram account has gained 20 followers.")
+        alert("Your account has gained 20 followers.")
     }
 }
 
