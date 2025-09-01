5b3f884c53f0ebb73a05842b674f1da2c49c7463
Mikhail McCatty <mikhailmccatty12@gmail.com>
2025-09-01 13:36:58 -0400

    Add speed analysis lab

---
+++ Labs/Speed_Analysis/speed_analysis.js
@@ -0,0 +1,42 @@
+let testText = "The quick brown fox jumps over the lazy dog.";
+let startTime, endTime;
+let userInput = document.getElementById("userInput");
+
+function startTest() {
+// Set the test text
+	document.getElementById("inputText").value = testText;
+// Reset user input and output
+
+	userInput.value = "";
+	userInput.readOnly = false;
+	userInput.focus();
+
+	document.getElementById("output").value = "";
+// Start timer
+	startTime = new Date().getTime();
+}
+
+function endTest() {
+	endTime = new Date().getTime();
+
+	userInput.readOnly = true;
+	const timeElapsed = (endTime - startTime)/1000;
+	const userTypedText = userInput.value;
+
+	const typedWords = userTypedText.split(/\s+/).filter(function (word) {
+		return word !== "";
+	}).length;
+
+	let wpm = 0;
+	if (timeElapsed !== 0 && !isNaN(typedWords)) {
+		wpm = Math.round(typedWords / timeElapsed * 60) ;
+	}
+
+	const outputDiv = document.getElementById("output");
+	outputDiv.innerHTML =
+	`<h2>Typing Test Results</h2> 
+	<p>Time Elapsed: ${timeElapsed.toFixed(2)} seconds</p>
+	<p> Words: ${typedWords}</p>
+	<p>Words per Minute (WPM): ${wpm} </p>`;
+}
+
